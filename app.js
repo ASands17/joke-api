451c3372cf800ca3b6e9310af8d4e3b91dd53d41
Amanda Sands <amandajean555@gmail.com>
2022-08-01 23:03:11 -0600

    working on cors error

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const cors = require('cors');
 const jokeRoute = require('./routes/jokes')
 
 express()
+  .use(cors())
   .use('/joke',jokeRoute)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
