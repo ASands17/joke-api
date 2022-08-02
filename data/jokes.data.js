c40fc7de58fa10e8036be4ff3d6ea77a8db8048b
Amanda Sands <amandajean555@gmail.com>
2022-08-01 22:52:15 -0600

    Add jokes

--- data/jokes.data.js
+++ data/jokes.data.js
@@ -1,7 +1,14 @@
 const jokes = [
     'My wife told me to stop impersonating a flamingo. I had to put my foot down.', 
     'I went to buy some camo pants but couldn’t find any.',
-    'I failed math so many times at school, I can’t even count.'
+    'I failed math so many times at school, I can’t even count.',
+    'How does the man in the moon get his hair cut? Eclipse it.',
+    'What’s a dog’s favorite homework assignment? A lab report.',
+    'You can’t believe everything you hear—but you can repeat it.',
+    'What do you call a bear with no teeth? A gummy bear.',
+    'Give a man a fish, and he will eat for a day. Teach a man to fish, and he will sit in a boat and drink beer all day.',
+    'Why didn’t Han Solo enjoy his steak dinner? It was Chewie.',
+    'Where does a winemaker get his gossip? Through the grapevine.'
 ]
 
 module.exports = jokes;
