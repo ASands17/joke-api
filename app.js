0c3e6c84e8016f6731f26fa9db40577286c4437c
Amanda Sands <amandajean555@gmail.com>
2022-08-01 20:50:33 -0600

    working on the heroku deploy, corrected route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const jokeRoute = require('./routes/jokes')
 
 express()
-  .use(jokeRoute)
+  .use('/joke',jokeRoute)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 module.exports = app;
