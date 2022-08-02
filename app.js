95acfd87ed018eadafc4e5ffb94e4d008cb25558
Amanda Sands <amandajean555@gmail.com>
2022-08-01 20:49:18 -0600

    working on the heroku deploy

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var jokesRouter = require('./routes/jokes');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-var app = express();
+const jokeRoute = require('./routes/jokes')
 
-
-
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-
-app.use('/', indexRouter);
-app.use('/joke', jokesRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+express()
+  .use(jokeRoute)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 module.exports = app;
