575e5bbc8aaa2b5dbd9818d98a80697f716e644c
Amanda Sands <amandajean555@gmail.com>
2022-11-09 14:08:21 -0700

    Add vercel file and adjust formatting

--- data/jokes.data.js
+++ data/jokes.data.js
@@ -5,7 +5,7 @@ const jokes = [
     'How does the man in the moon get his hair cut? Eclipse it.',
     'What’s a dog’s favorite homework assignment? A lab report.',
     'You can’t believe everything you hear—but you can repeat it.',
-    'What do you call a bear with no teeth? A gummy bear.',
+    'What do you call a bear with no teeth? A gummy bear!',
     'Give a man a fish, and he will eat for a day. Teach a man to fish, and he will sit in a boat and drink beer all day.',
     'Why didn’t Han Solo enjoy his steak dinner? It was Chewie.',
     'Where does a winemaker get his gossip? Through the grapevine.'
