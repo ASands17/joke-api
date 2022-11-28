f2b8c0a71fc7faa07d89456c635b5cd58616eacf
Amanda Sands <93043035+ASands17@users.noreply.github.com>
2022-11-28 06:44:28 -0700

    Update jokes.data.js

--- data/jokes.data.js
+++ data/jokes.data.js
@@ -3,7 +3,7 @@ const jokes = [
     'I went to buy some camo pants but couldn’t find any.',
     'I failed math so many times at school, I can’t even count.',
     'How does the man in the moon get his hair cut? Eclipse it.',
-    'What’s a dog’s favorite homework assignment? A lab report.',
+    'What’s a dog’s favorite homework assignment? A lab report!',
     'You can’t believe everything you hear—but you can repeat it.',
     'What do you call a bear with no teeth? A gummy bear!',
     'Give a man a fish, and he will eat for a day. Teach a man to fish, and he will sit in a boat and drink beer all day.',
